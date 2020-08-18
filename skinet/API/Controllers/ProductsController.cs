55ee80969159c48349c766662666a4719c361eb8
digbiijayeeniPattanaik <sonurcks92@gmail.com>
2020-08-18 21:47:28 +0100

    Initial Commit

---
+++ skinet/API/Controllers/ProductsController.cs
@@ -0,0 +1,38 @@
+﻿using Infrastructure.Data;
+using Core.Entites;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _storeContext;
+
+        public ProductsController(StoreContext storeContext)
+        {
+            _storeContext = storeContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            var product = await _storeContext.Products.ToListAsync();
+            return Ok(product);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            var product = await _storeContext.Products.FindAsync(id);
+            if (product != null)
+                return Ok(product);
+            else
+                return NotFound("Product not found");
+        }
+    }
+}
---
+++ skinet/Core/Entites/Product.cs
@@ -0,0 +1,8 @@
+﻿namespace Core.Entites
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
