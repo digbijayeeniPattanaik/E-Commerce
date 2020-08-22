d50af8632e56da81ae77e270f9d715ac2501f482
digbiijayeeniPattanaik <sonurcks92@gmail.com>
2020-08-22 21:00:40 +0100

    Shopping cart count of items in the basket.

--- skinet/client/src/app/core/core.module.ts
+++ skinet/client/src/app/core/core.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 
-
-
 @NgModule({
   declarations: [NavBarComponent],
   imports: [
