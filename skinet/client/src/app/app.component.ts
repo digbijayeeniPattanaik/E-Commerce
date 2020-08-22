7c0bbcb903a8f424dd3971824041c07217ec1770
digbiijayeeniPattanaik <sonurcks92@gmail.com>
2020-08-22 14:51:32 +0100

    Nav-bar created and http call to the api implemented and subscribe the response by using observable

--- skinet/client/src/app/app.component.ts
+++ skinet/client/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {IProduct} from './models/product';
+import {IPagination} from './models/pagination';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Skinet';
+  products: IProduct[];
+  constructor(private http: HttpClient){
+    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe((response: IPagination) => {
+      this.products = response.data;
+      console.log(response);
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  ngOnInit(): void {
+
+  }
 }
---
+++ skinet/client/src/app/dist/app.module.js
@@ -0,0 +1,38 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+exports.__esModule = true;
+exports.AppModule = void 0;
+var platform_browser_1 = require("@angular/platform-browser");
+var core_1 = require("@angular/core");
+var http_1 = require("@angular/common/http");
+var app_routing_module_1 = require("./app-routing.module");
+var app_component_1 = require("./app.component");
+var animations_1 = require("@angular/platform-browser/animations");
+var nav_bar_component_1 = require("./nav-bar/nav-bar.component");
+var AppModule = /** @class */ (function () {
+    function AppModule() {
+    }
+    AppModule = __decorate([
+        core_1.NgModule({
+            declarations: [
+                app_component_1.AppComponent,
+                nav_bar_component_1.NavBarComponent
+            ],
+            imports: [
+                platform_browser_1.BrowserModule,
+                app_routing_module_1.AppRoutingModule,
+                animations_1.BrowserAnimationsModule,
+                http_1.HttpClientModule
+            ],
+            providers: [],
+            bootstrap: [app_component_1.AppComponent]
+        })
+    ], AppModule);
+    return AppModule;
+}());
+exports.AppModule = AppModule;
