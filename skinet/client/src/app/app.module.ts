180dfdcb3b0167f66874e27f680987c09628a99d
digbiijayeeniPattanaik <sonurcks92@gmail.com>
2020-08-25 08:06:30 +0100

    Loading implemented using ngx-spinner and a delay interceptor added.

--- skinet/client/src/app/app.module.ts
+++ skinet/client/src/app/app.module.ts
@@ -8,6 +8,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module';
 import { HomeModule } from './home/home.module';
 import { ErrorInterceptor } from './core/interceptors/error.interceptor';
+import { LoadingInterceptor } from './core/interceptors/loading.interceptor';
+import { NgxSpinnerModule } from 'ngx-spinner';
 
 @NgModule({
   declarations: [
@@ -19,9 +21,13 @@ import { ErrorInterceptor } from './core/interceptors/error.interceptor';
     BrowserAnimationsModule,
     HttpClientModule,
     CoreModule,
-    HomeModule
+    HomeModule,
+    NgxSpinnerModule
   ],
-  providers: [{provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi:true}],
+  providers: 
+  [{provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi:true},
+  {provide: HTTP_INTERCEPTORS, useClass:LoadingInterceptor, multi:true}],
+  
   bootstrap: [AppComponent]
 })
 export class AppModule { }
