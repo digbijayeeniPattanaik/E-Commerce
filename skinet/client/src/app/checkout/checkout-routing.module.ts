e8a6cc37ad67fed655015ebadb3499d89a8da637
digbiijayeeniPattanaik <sonurcks92@gmail.com>
2020-08-27 10:41:20 +0100

    Created Checkout feature basic. Module, service , routing and component implemented.

---
+++ skinet/client/src/app/checkout/checkout-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CheckoutComponent } from './checkout.component';
+
+
+import { Routes, RouterModule } from '@angular/router';
+
+const routes : Routes = [
+  {path: '', component: CheckoutComponent }
+]
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class CheckoutRoutingModule { }
