6edfd66fb90eada8de84120070412e3d29540f77
digbiijayeeniPattanaik <sonurcks92@gmail.com>
2020-08-27 13:27:07 +0100

    Login the user basic implemneted API.

---
+++ skinet/API/Controllers/AccountController.cs
@@ -0,0 +1,41 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using API.Dtos;
+using API.Errors;
+using Core.Entites.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class AccountController : BaseApiController
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+        {
+            var user = await _userManager.FindByEmailAsync(loginDto.Email);
+
+            if (user == null) return Unauthorized(new ApiResponse((int)HttpStatusCode.Unauthorized));
+
+            var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+
+            if(!result.Succeeded) return Unauthorized(new ApiResponse((int)HttpStatusCode.Unauthorized));
+
+            return new UserDto
+            {
+                Email = loginDto.Email,
+                Token = "This will be a token",
+                DisplayName = user.DisplayName
+            };
+        }
+    }
+}
