a5e6d95aa2645050a061a6aa572211fc1459f2c1
digbiijayeeniPattanaik <sonurcks92@gmail.com>
2020-08-27 10:31:13 +0100

    Product details page modified and integrated with Basket service to add product to the basket.

--- skinet/client/src/app/shop/product-details/product-details.component.ts
+++ skinet/client/src/app/shop/product-details/product-details.component.ts
@@ -3,15 +3,23 @@ import { ShopService } from '../shop.service';
 import { IProduct } from 'src/app/shared/models/product';
 import { ActivatedRoute } from '@angular/router';
 import { BreadcrumbService } from 'xng-breadcrumb';
+import { BasketService } from 'src/app/basket/basket.service';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.scss']
+  styleUrls: ['./product-details.component.scss'],
 })
 export class ProductDetailsComponent implements OnInit {
   product: IProduct;
-  constructor(private shopService: ShopService, private activatedRoute: ActivatedRoute, private bcService: BreadcrumbService) { 
+  quantity = 1;
+
+  constructor(
+    private shopService: ShopService,
+    private activatedRoute: ActivatedRoute,
+    private bcService: BreadcrumbService,
+    private basketService: BasketService
+  ) {
     this.bcService.set('@productDetails', '');
   }
 
@@ -19,14 +27,33 @@ export class ProductDetailsComponent implements OnInit {
     this.loadProduct();
   }
 
-  loadProduct(){
+addItemToBasket() {
+  this.basketService.addItemToBasket(this.product, this.quantity);
+}
+
+incrementQuantity(){
+  this.quantity++;
+}
+
+decrementQuantity(){
+  if(this.quantity > 1){
+  this.quantity--;
+}
+}
+
+
+  loadProduct() {
     //name mentioned in the routing module
-    this.shopService.getProduct(+this.activatedRoute.snapshot.paramMap.get('id')).subscribe((response) => {
-      this.product = response;
-      this.bcService.set('@productDetails',this.product.name)
-    },
-    (error) => {
-      console.error(error);
-    })
+    this.shopService
+      .getProduct(+this.activatedRoute.snapshot.paramMap.get('id'))
+      .subscribe(
+        (response) => {
+          this.product = response;
+          this.bcService.set('@productDetails', this.product.name);
+        },
+        (error) => {
+          console.error(error);
+        }
+      );
   }
 }
