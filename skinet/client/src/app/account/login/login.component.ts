abc7459e2002dca1b4c90858f092271f881b9dc7
digbiijayeeniPattanaik <sonurcks92@gmail.com>
2020-08-28 16:44:51 +0100

    Pass data from formControl to Account service get a response of token.

--- skinet/client/src/app/account/login/login.component.ts
+++ skinet/client/src/app/account/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { AccountService } from '../account.service';
 
 @Component({
   selector: 'app-login',
@@ -9,7 +10,7 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
 
-  constructor() { }
+  constructor(private accountService : AccountService) { }
 
   ngOnInit(): void {
     this.createLoginForm();
@@ -23,6 +24,10 @@ export class LoginComponent implements OnInit {
   }
 
   onSubmit(){
-    console.log(this.loginForm.value);
+   this.accountService.login(this.loginForm.value).subscribe((response) => {
+     console.log('user logged in');
+   }, error => {
+     console.log(error);
+   })
   }
 }
