c00ca9fe3cbc1cfc81dac728c2cd0575dc9995fc
digbiijayeeniPattanaik <sonurcks92@gmail.com>
2020-08-28 20:05:03 +0100

    Created a custom component to have input field with validation errors and reused by all the other form. This would reduce the number of lines of code in HTML. Important

--- skinet/client/src/app/shared/shared.module.ts
+++ skinet/client/src/app/shared/shared.module.ts
@@ -7,10 +7,11 @@ import { PagerComponent } from './components/pager/pager.component';
 import { OrderTotalsComponent } from './components/order-totals/order-totals.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TextInputComponent } from './components/text-input/text-input.component';
 
 
 @NgModule({
-  declarations: [PagingHeaderComponent, PagerComponent, OrderTotalsComponent],
+  declarations: [PagingHeaderComponent, PagerComponent, OrderTotalsComponent, TextInputComponent],
   imports: [
     CommonModule,
     PaginationModule.forRoot(),
@@ -25,7 +26,8 @@ import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
     CarouselModule,
     OrderTotalsComponent,
     ReactiveFormsModule,
-    BsDropdownModule
+    BsDropdownModule,
+    TextInputComponent
   ]
 })
 export class SharedModule { }
