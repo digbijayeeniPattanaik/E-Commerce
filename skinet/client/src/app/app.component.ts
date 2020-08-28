d4b99a01c5ffe03a17706b9981dfa210e7146471
digbiijayeeniPattanaik <sonurcks92@gmail.com>
2020-08-28 17:22:16 +0100

    Loading the logged in user even if the page is refreshed.

--- skinet/client/src/app/app.component.ts
+++ skinet/client/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {IProduct} from './shared/models/product';
 import { BasketService } from './basket/basket.service';
+import { AccountService } from './account/account.service';
 
 @Component({
   selector: 'app-root',
@@ -10,17 +11,34 @@ import { BasketService } from './basket/basket.service';
 export class AppComponent implements OnInit {
   title = 'Skinet';
   products: IProduct[];
-  constructor(private basketService : BasketService){
+  constructor(private basketService : BasketService, private accountService : AccountService){
   }
 
   ngOnInit(): void {
+    this.loadBasket();
+    this.loadCurrentUser();
+  }
+
+  loadBasket(){
     const basketId = localStorage.getItem('basket_id');
     if(basketId){
       this.basketService.getBasket(basketId).subscribe(() => {
         console.log('Initialised Basket');
       }, error => {
         console.log(error);
-      })
+      });
+    }
+  }
+
+  loadCurrentUser(){
+    const token =localStorage.getItem('token');
+    if(token)
+    {
+    this.accountService.loadCurrentUser(token).subscribe(() => {
+      console.log('loaded user');
+    }, error => {
+      console.log(error);
+    })
     }
   }
 }
