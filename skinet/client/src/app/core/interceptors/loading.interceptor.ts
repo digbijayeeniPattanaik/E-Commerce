1bc8d98a6d656ab6ef8ed4cfe5ad5933f0827909
digbiijayeeniPattanaik <sonurcks92@gmail.com>
2020-08-28 22:25:45 +0100

    Implemented loading spinner in the text box while checking the email address if it exists.

--- skinet/client/src/app/core/interceptors/loading.interceptor.ts
+++ skinet/client/src/app/core/interceptors/loading.interceptor.ts
@@ -3,19 +3,23 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { delay, finalize} from 'rxjs/operators';
+import { delay, finalize } from 'rxjs/operators';
 import { BusyService } from '../services/busy.service';
 
 @Injectable()
 export class LoadingInterceptor implements HttpInterceptor {
+  constructor(private busyService: BusyService) {}
 
-  constructor(private busyService : BusyService) {}
-
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    this.busyService.busy();
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
+    if (!request.url.includes('emailexists')) {
+      this.busyService.busy();
+    }
     return next.handle(request).pipe(
       delay(1000),
       finalize(() => {
