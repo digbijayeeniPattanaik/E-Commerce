a703f733a43bdf11583cd53a03adc91aaa970a62
digbiijayeeniPattanaik <sonurcks92@gmail.com>
2020-08-28 17:09:48 +0100

    Display welcome user after login is successful.

--- skinet/client/src/app/core/nav-bar/nav-bar.component.ts
+++ skinet/client/src/app/core/nav-bar/nav-bar.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { IBasket } from 'src/app/shared/models/basket';
 import { BasketService } from 'src/app/basket/basket.service';
+import { IUser } from 'src/app/shared/models/user';
+import { AccountService } from 'src/app/account/account.service';
 
 @Component({
   selector: 'app-nav-bar',
@@ -10,10 +12,13 @@ import { BasketService } from 'src/app/basket/basket.service';
 })
 export class NavBarComponent implements OnInit {
 basket$: Observable<IBasket>;
-  constructor(private basketService : BasketService) { }
+currentUser$: Observable<IUser>;
+
+  constructor(private basketService : BasketService, private accountService : AccountService) { }
 
   ngOnInit(): void {
     this.basket$ = this.basketService.basket$;
+    this.currentUser$ = this.accountService.currentUser$;
   }
 
 }
