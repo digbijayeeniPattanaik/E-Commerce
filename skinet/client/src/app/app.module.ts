20032976c674208cd1c3d2512853644d74c9a319
digbiijayeeniPattanaik <sonurcks92@gmail.com>
2020-08-29 17:47:51 +0100

    JWT interceptor implmented to send the token for every request instead of sending invidually for all request.

--- skinet/client/src/app/app.module.ts
+++ skinet/client/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { CoreModule } from './core/core.module';
 import { HomeModule } from './home/home.module';
 import { ErrorInterceptor } from './core/interceptors/error.interceptor';
 import { LoadingInterceptor } from './core/interceptors/loading.interceptor';
+import { JwtInterceptor } from './core/interceptors/jwt.interceptor';
 import { NgxSpinnerModule } from 'ngx-spinner';
 import { BasketModule } from './basket/basket.module';
 
@@ -28,8 +29,8 @@ import { BasketModule } from './basket/basket.module';
   ],
   providers: 
   [{provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi:true},
-  {provide: HTTP_INTERCEPTORS, useClass:LoadingInterceptor, multi:true}],
-  
+  {provide: HTTP_INTERCEPTORS, useClass:LoadingInterceptor, multi:true},
+  {provide: HTTP_INTERCEPTORS, useClass:JwtInterceptor, multi:true}],  
   bootstrap: [AppComponent]
 })
 export class AppModule { }
