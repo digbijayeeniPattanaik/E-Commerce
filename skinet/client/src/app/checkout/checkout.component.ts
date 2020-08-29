719f6f71db69ccd9d7595ee5bf4c41b8072e087a
digbiijayeeniPattanaik <sonurcks92@gmail.com>
2020-08-29 14:48:32 +0100

    Defined form group for all the stepper pages.

--- skinet/client/src/app/checkout/checkout.component.ts
+++ skinet/client/src/app/checkout/checkout.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
-  styleUrls: ['./checkout.component.scss']
+  styleUrls: ['./checkout.component.scss'],
 })
 export class CheckoutComponent implements OnInit {
+  checkOutForm: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.createCheckOutForm()
   }
 
+  createCheckOutForm() {
+    this.checkOutForm = this.fb.group({
+      addressForm: this.fb.group({
+        firstName: [null, Validators.required],
+        lastName: [null, Validators.required],
+        street: [null, Validators.required],
+        city: [null, Validators.required],
+        state: [null, Validators.required],
+        zipcode: [null, Validators.required],
+      }),
+      deliveryForm: this.fb.group({
+        deliveryMethod: [null, Validators.required],
+      }),
+      paymentForm: this.fb.group({
+        nameOnCard: [null, Validators.required],
+      }),
+    });
+  }
 }
